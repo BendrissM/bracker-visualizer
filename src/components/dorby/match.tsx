225c0a676985517f02e1a08bda6c5e87cadec891
BendrissM <moncef-2006@live.fr>
2024-04-23 23:47:30 +0200

    add page dorby round 1 UI

---
+++ src/components/dorby/match.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import ReactCountryFlag from "react-country-flag";
+
+const Match = () => {
+  return (
+    <div className="h-[17rem] w-[20rem] rounded-2xl border-[0.5rem] border-[#e5afa1] bg-white bg-opacity-60 p-4">
+      <div>
+        <div className="flex flex-col gap-5">
+          <div className="mb-2 flex justify-between">
+            <span className="font-smibold text-lg">Match 1</span>
+            <span className="font-smibold text-lg">Place</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="font-smibold text-lg">
+              <ReactCountryFlag
+                countryCode="DE"
+                svg
+                style={{
+                  width: "1.5em",
+                  height: "1em",
+                  marginRight: "0.5em",
+                  borderRadius: "0.25em",
+                }}
+              />
+              Player 1
+            </span>
+            <span className="font-smibold min-w-12 text-center text-lg text-green-700">
+              1st
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span className="font-smibold text-lg">
+              <ReactCountryFlag
+                countryCode="DE"
+                svg
+                style={{
+                  width: "1.5em",
+                  height: "1em",
+                  marginRight: "0.5em",
+                  borderRadius: "0.25em",
+                }}
+              />
+              Player 1
+            </span>
+            <span className="font-smibold min-w-12 text-center text-lg text-red-600">
+              2nd
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span className="font-smibold text-lg">
+              <ReactCountryFlag
+                countryCode="DE"
+                svg
+                style={{
+                  width: "1.5em",
+                  height: "1em",
+                  marginRight: "0.5em",
+                  borderRadius: "0.25em",
+                }}
+              />
+              Player 1
+            </span>
+            <span className="font-smibold min-w-12 text-center text-lg text-red-600">
+              3rd
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span className="font-smibold text-lg">
+              <ReactCountryFlag
+                countryCode="DE"
+                svg
+                style={{
+                  width: "1.5em",
+                  height: "1em",
+                  marginRight: "0.5em",
+                  borderRadius: "0.25em",
+                }}
+              />
+              Player 1
+            </span>
+            <span className="font-smibold min-w-12 text-center text-lg text-red-600">
+              4th
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Match;
--- src/pages/dobry/r1/index.tsx
+++ src/pages/dobry/r1/index.tsx
@@ -1,13 +1,10 @@
-import RoundComponent from "@/components/round";
+import Group from "@/components/dorby/group";
 import { getGoogleSheetData } from "@/services/googleSheetService";
-import type { Round } from "@/types/Round";
 import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
-  useQuery,
 } from "@tanstack/react-query";
-import axios from "axios";
 import { type InferGetServerSidePropsType } from "next";
 import Head from "next/head";
 
@@ -31,28 +28,36 @@ export async function getServerSideProps() {
 export default function Home({
   dehydratedState,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const { data } = useQuery({
-    queryKey: ["swiss-rounds"],
-    queryFn: async () => {
-      const response = await axios.get<Round[]>("/api/googleSheet");
-      return response.data;
-    },
-    // refetchInterval: intervalMs,
-  });
+  //   const { data } = useQuery({
+  //     queryKey: ["swiss-rounds"],
+  //     queryFn: async () => {
+  //       const response = await axios.get<Round[]>("/api/googleSheet");
+  //       return response.data;
+  //     },
+  //     // refetchInterval: intervalMs,
+  //   });
 
   return (
     <HydrationBoundary state={dehydratedState}>
       <Head>
-        <title>MEATC Swiss Bracket</title>
+        <title>DOBRY Swiss Bracket</title>
         <meta name="description" content="created by benoxy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col justify-end bg-transparent bg-cover bg-repeat-round">
-        <div className="flex flex-row gap-12 pb-12 pt-3">
-          <div className="flex w-full items-center justify-center gap-32 px-28">
-            {data?.map((round, idx) => (
-              <RoundComponent round={round} key={idx} />
-            ))}
+      <main className="flex min-h-screen w-full bg-green-400">
+        <div className="flex w-full flex-row gap-12">
+          <div className="flex w-full items-center justify-center gap-[5.5rem]">
+            <Group />
+            <div className="flex">
+              <div className="mt-20">
+                <Group />
+              </div>
+              <h1 className="text-center text-4xl font-bold text-white mx-3 pt-5">0-0</h1>
+              <div className="mt-20">
+                <Group />
+              </div>
+            </div>
+            <Group />
           </div>
         </div>
       </main>
